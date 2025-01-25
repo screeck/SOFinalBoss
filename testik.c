4cd67805abdd0605417dfcb2c108e93015a045a9
KapitanOrbi <58605693+KapitanOrbi@users.noreply.github.com>
2025-01-25 20:37:59 +0100

    Update testik.c

--- testik.c
+++ testik.c
@@ -14,7 +14,6 @@
 // ---------------------------------
 // Konfiguracje
 // ---------------------------------
-
 #define SHM_SIZE 1024
 
 // Struktura wiadomości w kolejce
@@ -23,11 +22,11 @@ struct msgBuf {
     int  signo;    // numer sygnału (np. SIGTSTP = 20)
 };
 
-// Zmienne globalne (dla uproszczenia przykładu)
-int  msgid;        // ID kolejki komunikatów
-int  shmid;        // ID pamięci współdzielonej
-pid_t mainPID;     // PID procesu macierzystego
-pid_t p1PID, p2PID, p3PID; // PID-y procesów
+// Zmienne globalne
+int  msgid;      // ID kolejki komunikatów
+int  shmid;      // ID pamięci współdzielonej
+pid_t mainPID;   // PID procesu macierzystego
+pid_t p1PID, p2PID, p3PID; // PID-y procesów 1,2,3
 
 // Semafory do komunikacji p2 <-> p3
 sem_t *sem2 = NULL;
@@ -36,8 +35,6 @@ sem_t *sem3 = NULL;
 // ---------------------------------
 // Funkcje pomocnicze do kolejki
 // ---------------------------------
-
-// Wysłanie numeru sygnału do kolejki (o podanym typie)
 void sendSignalToQueue(int signo, long mtype) {
     struct msgBuf message;
     message.mtype = mtype;
@@ -47,7 +44,6 @@ void sendSignalToQueue(int signo, long mtype) {
     }
 }
 
-// Odczytanie numeru sygnału z kolejki (o podanym typie)
 int receiveSignalFromQueue(long mtype) {
     struct msgBuf message;
     if (msgrcv(msgid, &message, sizeof(message.signo), mtype, 0) == -1) {
@@ -60,7 +56,6 @@ int receiveSignalFromQueue(long mtype) {
 // ---------------------------------
 // Deklaracje handlerów sygnałów
 // ---------------------------------
-
 void sigusr2_handler_main(int signo);
 void sigusr1_handler_p1(int signo);
 void sigusr1_handler_p2(int signo);
@@ -70,18 +65,19 @@ void sigtstp_handler_p3(int signo);
 // ---------------------------------
 // Proces 1
 // ---------------------------------
-
 void process1(int fd_write) {
     p1PID = getpid();
+    // Każdy proces potomny w osobnej grupie:
+    setpgid(0, 0);
+
     printf("[Process 1] Starting (PID=%d)\n", p1PID);
 
-    // Ustawiamy handler sygnału SIGUSR1 (powiadomień z main)
+    // Obsługa SIGUSR1 (powiadomienie z Main)
     signal(SIGUSR1, sigusr1_handler_p1);
 
     char buffer[256];
     while (1) {
         if (!fgets(buffer, sizeof(buffer), stdin)) {
-            // fgets zwraca NULL przy EOF lub błędzie
             if (feof(stdin)) {
                 printf("[Process 1] End of input (EOF), closing pipe\n");
             } else {
@@ -106,21 +102,20 @@ void process1(int fd_write) {
 }
 
 // Handler w Procesie 1 dla SIGUSR1
+// Odczytuje sygnał z kolejki (typ=1), jeśli to SIGTSTP => przekazuje do p2 (typ=2), po czym sam się zatrzymuje.
 void sigusr1_handler_p1(int signo) {
-    printf("[Process 1] Received SIGUSR1 - reading from queue\n");
-
-    // Odczytujemy sygnał z kolejki (typ=1)
+    printf("[Process 1] Received SIGUSR1 -> reading from queue (mtype=1)\n");
     int sig_from_queue = receiveSignalFromQueue(1);
     if (sig_from_queue == SIGTSTP) {
         printf("[Process 1] Detected SIGTSTP in queue -> notifying Process 2\n");
 
-        // Wstawiamy znów do kolejki (tym razem mtype=2, żeby p2 mogło to odczytać)
+        // Wstawiamy ponownie do kolejki (typ=2), aby p2 mogło odczytać
         sendSignalToQueue(sig_from_queue, 2);
 
         // Powiadomienie procesu 2
         kill(p2PID, SIGUSR1);
 
-        // Zatrzymujemy się w tym miejscu
+        // Zatrzymanie (SIGSTOP) tylko procesu 1
         printf("[Process 1] Stopping (SIGSTOP)\n");
         kill(getpid(), SIGSTOP);
     }
@@ -129,12 +124,14 @@ void sigusr1_handler_p1(int signo) {
 // ---------------------------------
 // Proces 2
 // ---------------------------------
-
 void process2(int fd_read) {
     p2PID = getpid();
+    // Każdy proces potomny w osobnej grupie:
+    setpgid(0, 0);
+
     printf("[Process 2] Starting (PID=%d)\n", p2PID);
 
-    // Ustawiamy handler sygnału SIGUSR1
+    // Obsługa SIGUSR1
     signal(SIGUSR1, sigusr1_handler_p2);
 
     // Inicjalizacja pamięci współdzielonej
@@ -191,10 +188,9 @@ void process2(int fd_read) {
 }
 
 // Handler w Procesie 2 dla SIGUSR1
+// Odczytuje SIGTSTP (typ=2) -> przekazuje do p3 (typ=3), a następnie SIGSTOP.
 void sigusr1_handler_p2(int signo) {
-    printf("[Process 2] Received SIGUSR1 - reading from queue\n");
-
-    // Odczytujemy sygnał z kolejki (typ=2)
+    printf("[Process 2] Received SIGUSR1 -> reading from queue (mtype=2)\n");
     int sig_from_queue = receiveSignalFromQueue(2);
     if (sig_from_queue == SIGTSTP) {
         printf("[Process 2] Detected SIGTSTP in queue -> notifying Process 3\n");
@@ -214,21 +210,22 @@ void sigusr1_handler_p2(int signo) {
 // ---------------------------------
 // Proces 3
 // ---------------------------------
-
 void process3() {
     p3PID = getpid();
+    // Każdy proces potomny w osobnej grupie:
+    setpgid(0, 0);
+
     printf("[Process 3] Starting (PID=%d)\n", p3PID);
 
-    // Obsługa sygnałów: SIGUSR1 (łańcuch powiadomień) i SIGTSTP (od użytkownika)
-    signal(SIGUSR1, sigusr1_handler_p3);
-
-    // Domyślnie SIGTSTP zatrzymałby proces, ale chcemy własną akcję:
+    // Chcemy własną obsługę SIGTSTP (zamiast domyślnej)
     struct sigaction sa;
     memset(&sa, 0, sizeof(sa));
     sa.sa_handler = sigtstp_handler_p3;  
-    // Nie dopuszczamy do domyślnego zatrzymania przez kernel
-    // bo chcemy przeprowadzić specjalny mechanizm.
+    // "Zastąp" domyślne zatrzymywanie przez nasz handler
     sigaction(SIGTSTP, &sa, NULL);
+
+    // Obsługa SIGUSR1 (ostatni etap łańcucha zatrzymania)
+    signal(SIGUSR1, sigusr1_handler_p3);
 
     // Pamięć współdzielona
     shmid = shmget(ftok("shmfile", 65), SHM_SIZE, 0666 | IPC_CREAT);
@@ -244,7 +241,6 @@ void process3() {
     }
     printf("[Process 3] Shared memory attached\n");
 
-    // Semafory
     sem2 = sem_open("/sem2", O_CREAT, 0666, 0);
     sem3 = sem_open("/sem3", O_CREAT, 0666, 0);
     if (sem2 == SEM_FAILED || sem3 == SEM_FAILED) {
@@ -271,37 +267,36 @@ void process3() {
 }
 
 // Handler w Procesie 3 (SIGTSTP)
-// Zamiast zatrzymywać się domyślnie, wysyłamy SIGUSR2 do main
+// Zamiast zatrzymywać się od razu, wysyłamy SIGUSR2 do Main
 void sigtstp_handler_p3(int signo) {
     printf("[Process 3] Received SIGTSTP from user -> sending SIGUSR2 to Main (PID=%d)\n",
            mainPID);
     kill(mainPID, SIGUSR2);
-    // Nie zatrzymujemy się tutaj! Cała sekwencja wstrzymania nastąpi
-    // przez kolejkę komunikatów i sygnały SIGUSR1 (1->2->3).
+    // UWAGA: nie wykonujemy tu STOPa! (Zatrzymanie nastąpi dopiero
+    // poprzez kolejkę i łańcuch sygnałów 1->2->3)
 }
 
-// Handler w Procesie 3 (SIGUSR1) - ostatni etap łańcucha
+// Handler w Procesie 3 (SIGUSR1)
+// Odczytuje SIGTSTP (typ=3) i dopiero wtedy się zatrzymuje
 void sigusr1_handler_p3(int signo) {
-    printf("[Process 3] Received SIGUSR1 - reading from queue\n");
-
-    // Odczytujemy sygnał z kolejki (typ=3)
+    printf("[Process 3] Received SIGUSR1 -> reading from queue (mtype=3)\n");
     int sig_from_queue = receiveSignalFromQueue(3);
     if (sig_from_queue == SIGTSTP) {
-        printf("[Process 3] Detected SIGTSTP in queue -> stopping now.\n");
+        printf("[Process 3] Detected SIGTSTP in queue -> stopping now (SIGSTOP)\n");
         kill(getpid(), SIGSTOP);
     }
 }
 
 // ---------------------------------
 // Handler w Main dla SIGUSR2
-// (wysyłany przez Proces 3 po otrzymaniu SIGTSTP)
+// (wysyłany przez Proces 3 po otrzymaniu SIGTSTP "z zewnątrz")
 void sigusr2_handler_main(int signo) {
     printf("[Main] Received SIGUSR2 from Process 3 -> writing SIGTSTP to queue (mtype=1)\n");
 
-    // Wstawiamy informację o sygnale SIGTSTP do kolejki
+    // Wstawiamy informację o sygnale SIGTSTP (20) do kolejki
     sendSignalToQueue(SIGTSTP, 1);
 
-    // Powiadamiamy proces 1, by odczytał kolejkę
+    // Powiadamiamy proces 1, by odczytał kolejkę (typ=1)
     printf("[Main] Notifying Process 1 (SIGUSR1)\n");
     kill(p1PID, SIGUSR1);
 }
@@ -309,12 +304,11 @@ void sigusr2_handler_main(int signo) {
 // ---------------------------------
 // main
 // ---------------------------------
-
 int main() {
     printf("[Main] Starting main process (PID=%d)\n", getpid());
-    mainPID = getpid();  // zapamiętujemy PID procesu głównego
+    mainPID = getpid();
 
-    // Kolejka komunikatów
+    // Tworzymy kolejkę komunikatów
     key_t msgKey = ftok("msgqueue", 65);
     msgid = msgget(msgKey, 0666 | IPC_CREAT);
     if (msgid == -1) {
@@ -322,7 +316,7 @@ int main() {
         exit(1);
     }
 
-    // Handler dla sygnału SIGUSR2 (odbierany od procesów potomnych, gł. p3)
+    // Handler dla sygnału SIGUSR2 (odbierany od p3)
     signal(SIGUSR2, sigusr2_handler_main);
 
     // Tworzymy potok
@@ -335,7 +329,7 @@ int main() {
     // Uruchamiamy proces 1
     pid_t pid1 = fork();
     if (pid1 == 0) {
-        close(pipe_fd[0]);   // w p1 nie czytamy
+        close(pipe_fd[0]);
         process1(pipe_fd[1]);
     }
     p1PID = pid1;
@@ -343,7 +337,7 @@ int main() {
     // Uruchamiamy proces 2
     pid_t pid2 = fork();
     if (pid2 == 0) {
-        close(pipe_fd[1]);   // w p2 nie piszemy
+        close(pipe_fd[1]);
         process2(pipe_fd[0]);
     }
     p2PID = pid2;
@@ -351,15 +345,15 @@ int main() {
     // Uruchamiamy proces 3
     pid_t pid3 = fork();
     if (pid3 == 0) {
-        // Tutaj nie korzystamy z potoku w p3, więc nie trzeba zamykać
+        // W p3 nie korzystamy z potoku
         process3();
     }
     p3PID = pid3;
 
-    // Proces macierzysty
     close(pipe_fd[0]);
     close(pipe_fd[1]);
 
+    // Proces macierzysty pozostaje aktywny
     // Czekamy na zakończenie procesów potomnych
     waitpid(pid1, NULL, 0);
     printf("[Main] Process 1 finished\n");
@@ -370,14 +364,14 @@ int main() {
     waitpid(pid3, NULL, 0);
     printf("[Main] Process 3 finished\n");
 
-    // Usunięcie semaforów
+    // Usuwamy semafory
     sem_unlink("/sem2");
     sem_unlink("/sem3");
 
-    // Usunięcie pamięci współdzielonej
+    // Usuwamy pamięć współdzieloną
     shmctl(shmget(ftok("shmfile", 65), SHM_SIZE, 0666 | IPC_CREAT), IPC_RMID, NULL);
 
-    // Usunięcie kolejki komunikatów
+    // Usuwamy kolejkę komunikatów
     msgctl(msgid, IPC_RMID, NULL);
 
     printf("[Main] Cleaned up resources and exiting\n");
