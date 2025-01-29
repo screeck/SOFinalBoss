ca5c3b24bea2c2dee561e2d21a774c7dfe52f56f
KapitanOrbi <58605693+KapitanOrbi@users.noreply.github.com>
2025-01-29 18:30:13 +0100

    Update deepkodzik.c

--- deepkodzik.c
+++ deepkodzik.c
@@ -26,16 +26,8 @@ static int shm_id;
 static sem_t *sem;
 static pid_t p1, p2, p3;
 
-// Struktury dla flag pauzy
-struct process_data {
-    volatile sig_atomic_t paused;
-};
-
-struct process_data p1_data = {0}, p2_data = {0};
-
 // --- Funkcja sprzątająca ---
 void cleanup() {
-    printf("\n[PARENT] Cleaning up resources\n");
     msgctl(msgq_id, IPC_RMID, NULL);
     shmctl(shm_id, IPC_RMID, NULL);
     sem_close(sem);
@@ -46,16 +38,19 @@ void cleanup() {
 void parent_sigusr1_handler(int sig) {
     msgbuf msg;
     msgrcv(msgq_id, &msg, sizeof(int), 1, 0);
-    printf("[PARENT] Received signal %d from P3\n", msg.signal);
+    const int received_signal = msg.signal;
 
-    if (msg.signal == SIGTSTP || msg.signal == SIGCONT) {
-        // Wysyłanie komend do P1 i P2
-        msgbuf cmd_p1 = {.mtype = 3, .signal = msg.signal};
-        msgbuf cmd_p2 = {.mtype = 4, .signal = msg.signal};
+    if (received_signal == SIGTSTP || received_signal == SIGCONT) {
+        msgbuf cmd_p1 = {.mtype = 3, .signal = received_signal};
+        msgbuf cmd_p2 = {.mtype = 4, .signal = received_signal};
         msgsnd(msgq_id, &cmd_p1, sizeof(int), 0);
         msgsnd(msgq_id, &cmd_p2, sizeof(int), 0);
+
+        if (received_signal == SIGCONT) {
+            kill(p1, SIGCONT);
+            kill(p2, SIGCONT);
+        }
         
-        // Powiadomienie procesów
         kill(p1, SIGUSR1);
         kill(p2, SIGUSR1);
     } else {
@@ -67,7 +62,6 @@ void parent_sigusr1_handler(int sig) {
 
 // --- Obsługa sygnałów w procesie P3 ---
 void sig_handler(int sig) {
-    printf("\n[P3] Received signal %d\n", sig);
     msgbuf msg = {.mtype = 1, .signal = sig};
     msgsnd(msgq_id, &msg, sizeof(int), 0);
     kill(getppid(), SIGUSR1);
@@ -82,9 +76,12 @@ void sig_handler(int sig) {
 void p1_signal_handler(int sig) {
     msgbuf msg;
     if (msgrcv(msgq_id, &msg, sizeof(int), 3, IPC_NOWAIT) != -1) {
-        printf("[P1] Received %s command\n", 
-              (msg.signal == SIGTSTP) ? "PAUSE" : "RESUME");
-        p1_data.paused = (msg.signal == SIGTSTP);
+        if (msg.signal == SIGTSTP) {
+            printf("\n[P1] PAUSING (SIGTSTP)\n");
+            raise(SIGSTOP);
+        } else if (msg.signal == SIGCONT) {
+            printf("\n[P1] RESUMING (SIGCONT)\n");
+        }
     } else {
         msgrcv(msgq_id, &msg, sizeof(int), 2, 0);
         printf("[P1] Forwarding signal %d to P2\n", msg.signal);
@@ -96,9 +93,12 @@ void p1_signal_handler(int sig) {
 void p2_signal_handler(int sig) {
     msgbuf msg;
     if (msgrcv(msgq_id, &msg, sizeof(int), 4, IPC_NOWAIT) != -1) {
-        printf("[P2] Received %s command\n", 
-              (msg.signal == SIGTSTP) ? "PAUSE" : "RESUME");
-        p2_data.paused = (msg.signal == SIGTSTP);
+        if (msg.signal == SIGTSTP) {
+            printf("\n[P2] PAUSING (SIGTSTP)\n");
+            raise(SIGSTOP);
+        } else if (msg.signal == SIGCONT) {
+            printf("\n[P2] RESUMING (SIGCONT)\n");
+        }
     }
 }
 
@@ -106,24 +106,24 @@ void p2_signal_handler(int sig) {
 void p1_signals() {
     struct sigaction sa;
     sa.sa_handler = p1_signal_handler;
-    sa.sa_flags = SA_RESTART;
     sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
     sigaction(SIGUSR1, &sa, NULL);
 
-    signal(SIGINT, SIG_IGN);
-    signal(SIGTERM, SIG_IGN);
+    signal(SIGTSTP, SIG_IGN);
+    signal(SIGCONT, SIG_IGN);
 }
 
 // --- Rejestracja sygnałów w P2 ---
 void p2_signals() {
     struct sigaction sa;
     sa.sa_handler = p2_signal_handler;
-    sa.sa_flags = SA_RESTART;
     sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
     sigaction(SIGUSR1, &sa, NULL);
 
-    signal(SIGINT, SIG_IGN);
-    signal(SIGTERM, SIG_IGN);
+    signal(SIGTSTP, SIG_IGN);
+    signal(SIGCONT, SIG_IGN);
 }
 
 // --- Proces P1 z menu ---
@@ -132,52 +132,50 @@ void run_p1() {
     p1_signals();
     close(pipe_fd[0]);
 
-    while(1) {
-        if (!p1_data.paused) {
-            printf("\n=== MENU (P1) ===\n");
-            printf("1. Wczytaj z klawiatury\n");
-            printf("2. Wczytaj z pliku\n");
-            printf("Wybor: ");
-            fflush(stdout);
+    char choice[10];
+    while (1) {
+        printf("\n=== MENU (P1) ===\n");
+        printf("1. Wczytaj z klawiatury\n");
+        printf("2. Wczytaj z pliku\n");
+        printf("Wybor: ");
+        fflush(stdout);
 
-            char choice[10];
-            fgets(choice, sizeof(choice), stdin);
-            int c = atoi(choice);
-
-            switch(c) {
-                case 1: {
-                    printf("Wprowadz dane (pusta linia konczy):\n");
+        if (fgets(choice, sizeof(choice), stdin) == NULL) break;
+        
+        switch (atoi(choice)) {
+            case 1: {
+                printf("Wprowadz dane (pusta linia konczy):\n");
+                char buffer[MAX_LINE];
+                while (fgets(buffer, MAX_LINE, stdin) && strcmp(buffer, "\n") != 0) {
+                    printf("[P1] Wyslano: %s", buffer);
+                    write(pipe_fd[1], buffer, strlen(buffer));
+                }
+                break;
+            }
+            case 2: {
+                printf("Podaj nazwe pliku: ");
+                char filename[256];
+                fgets(filename, sizeof(filename), stdin);
+                filename[strcspn(filename, "\n")] = 0;
+                
+                FILE *fp = fopen(filename, "r");
+                if (fp) {
                     char buffer[MAX_LINE];
-                    while(fgets(buffer, MAX_LINE, stdin) && strcmp(buffer, "\n") != 0) {
+                    while (fgets(buffer, MAX_LINE, fp)) {
+                        printf("[P1] Wyslano: %s", buffer);
                         write(pipe_fd[1], buffer, strlen(buffer));
                     }
-                    break;
+                    fclose(fp);
                 }
-                case 2: {
-                    printf("Podaj nazwe pliku: ");
-                    char filename[256];
-                    fgets(filename, sizeof(filename), stdin);
-                    filename[strcspn(filename, "\n")] = 0;
-                    
-                    FILE *fp = fopen(filename, "r");
-                    if(fp) {
-                        char buffer[MAX_LINE];
-                        while(fgets(buffer, MAX_LINE, fp)) {
-                            write(pipe_fd[1], buffer, strlen(buffer));
-                        }
-                        fclose(fp);
-                    }
-                    break;
-                }
-                default: {
-                    printf("Niepoprawny wybor!\n");
-                    break;
-                }
+                break;
             }
-        } else {
-            pause();
+            default: {
+                printf("Niepoprawny wybor!\n");
+                break;
+            }
         }
     }
+    exit(EXIT_SUCCESS);
 }
 
 // --- Proces P2 ---
@@ -189,22 +187,15 @@ void run_p2() {
     char buffer[MAX_LINE];
     int *shm_ptr = (int*)shmat(shm_id, NULL, 0);
 
-    while(1) {
-        if (!p2_data.paused) {
-            int bytes_read;
-            while((bytes_read = read(pipe_fd[0], buffer, MAX_LINE)) > 0) {
-                buffer[bytes_read] = 0;
-                *shm_ptr = strlen(buffer) - 1;
-                sem_post(sem);
-                printf("[P2] Processed %d bytes\n", bytes_read);
-            }
-            
-            if (bytes_read == -1 && errno != EINTR) {
-                break;
-            }
-        } else {
-            printf("[P2] PAUSED\n");
-            pause();
+    while (1) {
+        ssize_t bytes_read = read(pipe_fd[0], buffer, MAX_LINE);
+        if (bytes_read > 0) {
+            buffer[bytes_read] = 0;
+            printf("[P2] Odebrano: %s", buffer);
+            *shm_ptr = strlen(buffer) - 1;
+            sem_post(sem);
+        } else if (bytes_read == -1 && errno != EINTR) {
+            break;
         }
     }
     
@@ -221,44 +212,34 @@ void run_p3() {
     signal(SIGTERM, sig_handler);
 
     int *shm_ptr = (int*)shmat(shm_id, NULL, 0);
-    while(1) {
+    while (1) {
         sem_wait(sem);
-        printf("[P3] Licznik: %d\n", *shm_ptr);
+        printf("[P3] Liczba znakow: %d\n", *shm_ptr);
     }
 }
 
 // --- Funkcja main ---
 int main() {
-    printf("[PARENT] Starting (PID: %d)\n", getpid());
+    printf("[PARENT] Started (PID: %d)\n", getpid());
     atexit(cleanup);
 
-    // Inicjalizacja zasobów
     pipe(pipe_fd);
     msgq_id = msgget(IPC_PRIVATE, 0666 | IPC_CREAT);
     shm_id = shmget(IPC_PRIVATE, SHM_SIZE, 0666 | IPC_CREAT);
     sem = sem_open("/pcsem", O_CREAT, 0666, 0);
 
-    // Uruchamianie procesów
     p1 = fork();
-    if(p1 == 0) run_p1();
-    
+    if (p1 == 0) run_p1();
+
     p2 = fork();
-    if(p2 == 0) run_p2();
-    
+    if (p2 == 0) run_p2();
+
     p3 = fork();
-    if(p3 == 0) run_p3();
+    if (p3 == 0) run_p3();
 
-    // Konfiguracja handlera w rodzicu
     signal(SIGUSR1, parent_sigusr1_handler);
-    printf("[PARENT] Children PIDs: P1=%d, P2=%d, P3=%d\n", p1, p2, p3);
+    printf("[PARENT] Dzieci: P1=%d, P2=%d, P3=%d\n", p1, p2, p3);
 
-    // Pętla oczekiwania
-    int running = 3;
-    while(running > 0) {
-        pid_t pid = waitpid(-1, NULL, WNOHANG);
-        if(pid > 0) running--;
-        usleep(100000);
-    }
-
+    while (waitpid(-1, NULL, WNOHANG) == 0) pause();
     return EXIT_SUCCESS;
 }
